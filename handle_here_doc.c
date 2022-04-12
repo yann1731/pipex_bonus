a62c6d65d44995d865ce2099df07b24b20fa390a
Yannick St-laurent <yst-laur@c1r8p4.42quebec.com>
2022-04-12 14:06:29 -0400

    let's go

--- handle_here_doc.c
+++ handle_here_doc.c
@@ -7,9 +7,19 @@ void	handle_here_doc(char *argv[], t_data data)
 {
 	int fd[2];
 	int fd1;
+	char *str;
 
 	dup2(fd[0], STDIN_FILENO);
+	dup2(fd[1], STDOUT_FILENO);
 	close(fd[0]);
+	close(fd[1]);
 	fd1 = open(HERE_DOC, O_RDONLY);
-	
+	str = get_next_line(fd1);
+	while (ft_strnstrint(str, DELIMITER, ft_strlen(str)) == 0)
+	{
+		printf("%s", str);
+		free(str);
+		str = get_next_line(fd1);
+	}
+	free(str);
 }
