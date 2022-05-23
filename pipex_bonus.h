bd3e337673e275d5f84fb328b1a5a789aa91b448
Yannick St-laurent <yst-laur@c1r8p4.42quebec.com>
2022-05-23 11:29:15 -0400

    let's go

--- pipex_bonus.h
+++ pipex_bonus.h
@@ -25,7 +25,6 @@ void	checkargs(int argc);
 void	checkoutput(int output);
 char	*findpath(char **environ);
 char	**ft_split(const char *s, char c);
-char	*ft_strjoin(char const *s1, char const *s2);
 int		ft_strlen(const char *s);
 char	*ft_strstr(char *str, char *to_find);
 char	**get_path(void);
