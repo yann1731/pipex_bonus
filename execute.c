7d4a22a4b4242ed9efaa28cc08f06bceec1c2ffc
fenix144 <yan_1731@hotmail.com>
2022-05-26 17:06:25 -0400

    Moved malloc to execute.c depending on the case, either here_doc or standard arguments

--- execute.c
+++ execute.c
@@ -3,7 +3,13 @@
 void	execute(char *argv[], t_data data)
 {
 	if (ft_strnstrint(HERE_DOC, "here_doc", ft_strlen(HERE_DOC)) == 1)
-		handle_here_doc(argv, data);
+	{
+		data.pid = malloc((data.argc - 4) * sizeof(int));
+		handle_here_doc(argv, data);	
+	}
 	else
+	{
+		data.pid = malloc((data.argc - 3) * sizeof(int));
 		handle_in_out(argv, data);
+	}
 }
